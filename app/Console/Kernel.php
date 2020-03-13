84776ae67858702b71e36734639b04c99d902b7c
Cody Butz <cbutz@omnispear.com>
2020-03-13 00:04:03 -0400

    Another quick change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('lazy-trader:fetch-current-pricing-all')->hourly();
-        $schedule->command('cryptocompare:fetch-prices')->hourly();
+        $schedule->command('cryptocompare:fetch-prices --all')->hourly();
         $schedule->command('cryptocompare:fetch-news')->daily();
         $schedule->command('cryptocompare:fetch-news-by-source')->daily();
         $schedule->command('cryptocompare:fetch-exchanges')->weekly();
